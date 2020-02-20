236992b2bb418bbbb2c650c47df84073a06357f4
Zhiwei Han <hanzw356255531@icloud.com>
2020-02-20 20:47:53 +0100

    finish

--- app/pgat_recsys.py
+++ app/pgat_recsys.py
@@ -4,6 +4,9 @@ import numpy as np
 import pandas as pd
 import torch
 import os.path as osp
+import operator
+
+from torch_geometric.utils import path
 
 from torch_geometric.datasets import MovieLens
 
@@ -16,17 +19,14 @@ class PGATRecSys(object):
         self.train_args = train_args
 
         self.data = MovieLens(**dataset_args).data.to(train_args['device'])
-        model = PGATNetEx(
+        self.model = PGATNetEx(
                 self.data.num_nodes[0],
                 self.data.num_relations[0],
-                **model_args)
-        # if not dataset_args['debug']:
-        #     model.load_state_dict(torch.load(train_args))
-        self.model = model.to(train_args['device'])
+                **model_args
+        ).to(train_args['device'])
 
     def get_top_n_popular_items(self, n=10):
         """
-        TODO: Zhe
         Get the top n movies from self.data.ratings.
         Remove the duplicates in self.data.ratings and sort it by movie count.
         After you find the top N popular movies' item id,
@@ -38,7 +38,7 @@ class PGATRecSys(object):
 
         ratings_df = self.data.ratings[0][['iid', 'movie_count']]
         ratings_df = ratings_df.drop_duplicates()
-        ratings_df = ratings_df.sort_index(axis=0,by='movie_count',ascending=False)
+        ratings_df = ratings_df.sort_index(axis=0, by='movie_count', ascending=False)
 
         return ratings_df[:n]
 
@@ -49,62 +49,65 @@ class PGATRecSys(object):
         :param demographic_info: (gender, occupation), tuple
         :return:
         """
+        self.base_iids = iids
+        self.demographic_info = demographic_info
         # Build edges for new user
-
-        new_user_nid = self.model.node_emb.weight.shape[0]
-        new_user_gender_nid = self.data.gender_node_id_map[0][demographic_info[0]]
-        new_user_occ_nid = self.data.occupation_node_id_map[0][demographic_info[1]]
-        row = [new_user_nid for i in range(len(iids) + 2)]
+        self.new_user_nid = self.model.node_emb.weight.shape[0]
+        new_user_gender_nid = self.data.gender2nid[0][demographic_info[0]]
+        new_user_occ_nid = self.data.occ2nid[0][demographic_info[1]]
+        row = [self.new_user_nid for i in range(len(iids) + 2)]
         col = iids + [new_user_gender_nid, new_user_occ_nid]
-        new_edge_index_np = torch.from_numpy(np.array([row, col]))
-        new_edge_index = new_edge_index_np.long().to(self.train_args['device'])
+        self.new_edge_index = torch.from_numpy(np.array([row + col, col + row])).long().to(self.train_args['device'])
 
-        # Build second order edges
-        new_edge_index_df = pd.DataFrame({'head': new_edge_index_np[0, :], 'middle': new_edge_index_np[1, :]})
-        edge_index_np = self.data.edge_index.numpy()
-        edge_index_df = pd.DataFrame({'middle': edge_index_np[0, :], 'tail': edge_index_np[1, :]})
-        new_sec_order_edge_df = pd.merge(new_edge_index_df, edge_index_df, on='middle')
-        new_sec_order_edge_np = new_sec_order_edge_df.to_numpy()
-        new_sec_order_edge = torch.from_numpy(new_sec_order_edge_np).to(self.train_args['device']).t()
+        # Build path begins and ends with
+        path_from_new_user_np = path.join(self.new_edge_index, self.data.edge_index)
+        path_to_new_user_np = path.join(self.data.edge_index, self.new_edge_index)
+        new_path_np = np.concatenate([path_from_new_user_np, path_to_new_user_np], axis=1)
+        self.new_path = torch.from_numpy(new_path_np).to(self.train_args['device'])
+        # new_edge_index_df = pd.DataFrame({'head': new_edge_index_np[0, :], 'middle': new_edge_index_np[1, :]})
+        # edge_index_np = self.data.edge_index.numpy()
+        # edge_index_df = pd.DataFrame({'middle': edge_index_np[0, :], 'tail': edge_index_np[1, :]})
+        # new_sec_order_edge_df = pd.merge(new_edge_index_df, edge_index_df, on='middle')
+        # new_sec_order_edge_np = new_sec_order_edge_df.to_numpy()
+        # new_sec_order_edge = torch.from_numpy(new_sec_order_edge_np).to(self.train_args['device']).t()
 
         # Get new user embedding by applying message passing
-        node_emb = self.model.node_emb.weight
-        new_user_emb = torch.nn.Embedding(1, self.model.node_emb.weight.shape[1]).weight
-        node_emb = torch.cat((node_emb, new_user_emb), dim=0)
-        self.node_emb = self.model.forward_(node_emb, new_edge_index, new_sec_order_edge)
-        self.new_user_emb = self.model.forward_(node_emb, new_edge_index, new_sec_order_edge)[-1, :]
+        path = torch.from_numpy(self.data.path[0]).to(self.train_args['device'])
+        self.node_emb = self.model.forward_(self.model.node_emb.weight, path)
+        self.new_user_emb = torch.nn.Embedding(1, self.model.node_emb.weight.shape[1]).weight
+        node_emb = torch.cat((self.model.node_emb.weight, self.new_user_emb), dim=0)
+        self.new_user_emb, self.att_factor = self.model.forward_(node_emb, self.new_path)[-1, :]
         print('user building done...')
-
-    def extend_user(self, iids):
-        pass
 
     def get_recommendations(self, seen_iids):
         # Estimate the feedback values and get the recommendation
         iids = self.get_top_n_popular_items(200).iid
         rec_iids = [iid for iid in iids if iid not in seen_iids]
         rec_iids = np.random.choice(rec_iids, 20)
-        rec_nids = self.data.items[0].iloc[rec_iids].nid.values
+        rec_nids = [self.data.iid2nid[0][iid] for iid in rec_iids]
         rec_item_emb = self.node_emb[rec_nids]
         est_feedback = torch.sum(self.new_user_emb * rec_item_emb, dim=1).reshape(-1).cpu().detach().numpy()
         rec_iid_idx = np.argsort(est_feedback)[:self.num_recs]
         rec_iids = rec_iids[rec_iid_idx]
 
         df = self.data.items[0][self.data.items[0].iid.isin(rec_iids)]
+        iids = [iid for iid in df.iids.value]
 
-        exp = self.get_explanation(df)
+        exp = [self.get_explanation(iid) for iid in iids]
 
         return df, exp
 
-    def get_explanation(self, df):
-        return ["some exp." for i in range(len(df))]
-
-    # @staticmethod
-    # def get_weights_path(dataset_args, model_args, train_args):
-    #     params = 'hs{}_lr{}_core{}_reprdim{}'.format(
-    #         model_args['hidden_size'],
-    #         train_args['lr'], dataset_args['num_core'], model_args['repr_dim'])
-    #     if train_args['model'] == 'GAT' or train_args['model'] == 'PGAT':
-    #         params = 'heads{}_'.format(model_args['heads']) + params
-    #     debug = '' if not train_args['debug'] else '_debug{}'.format(train_args['debug'])
-    #     weights_path = osp.join(train_args['weights_folder'], params + debug)
-    #     return weights_path
+    def get_explanation(self, iid):
+        movie_nid = self.data.iid2nid[iid]
+        row = [self.new_user_nid, movie_nid]
+        col = [movie_nid, self.new_user_emb]
+        expl_edge_index = torch.from_numpy(np.array([row, col])).long().to(self.train_args['device'])
+        new_edge_index = torch.cat([self.data.edge_index, self.new_edge_index], axis=1)
+        expl_path_from_tuple = path.join(expl_edge_index, new_edge_index)
+        exp_path_to_tuple = path.join(new_edge_index, expl_edge_index)
+        exp_path = np.concatenate([expl_path_from_tuple, exp_path_to_tuple], axis=1)
+        node_emb = torch.cat((self.model.node_emb.weight, self.new_user_emb), dim=0)
+        self.new_user_emb, self.att_factor_dict = self.model.forward_(node_emb, exp_path)[-1, :]
+        path = max(self.att_factor_dict.iteritems(), key=operator.itemgetter(1))[0]
+        path_np = path.cpu().to_numpy()
+        return str(path_np)
---
+++ app/train.py
@@ -0,0 +1,67 @@
+import argparse
+import torch
+
+from .utils import get_folder_path
+
+__model__ = 'PGAT'
+
+parser = argparse.ArgumentParser()
+
+# Dataset params
+parser.add_argument("--dataset", type=str, default='movielens', help="")
+parser.add_argument("--dataset_name", type=str, default='1m', help="")
+parser.add_argument("--num_core", type=int, default=10, help="")
+parser.add_argument("--step_length", type=int, default=2, help="")
+parser.add_argument("--train_ratio", type=float, default=0.8, help="")
+parser.add_argument("--debug", default=0.01, help="")
+
+# Model params
+parser.add_argument("--heads", type=int, default=4, help="")
+parser.add_argument("--emb_dim", type=int, default=64, help="")
+parser.add_argument("--repr_dim", type=int, default=16, help="")
+
+# Train params
+parser.add_argument("--device", type=str, default='cuda', help="")
+parser.add_argument("--gpu_idx", type=str, default='0', help="")
+parser.add_argument("--runs", type=int, default=10, help="")
+parser.add_argument("--epochs", type=int, default=50, help="")
+parser.add_argument("--opt", type=str, default='adam', help="")
+parser.add_argument("--loss", type=str, default='mse', help="")
+parser.add_argument("--batch_size", type=int, default=256, help="")
+parser.add_argument("--lr", type=float, default=1e-4, help="")
+parser.add_argument("--weight_decay", type=float, default=0, help="")
+parser.add_argument("--early_stopping", type=int, default=40, help="")
+
+args = parser.parse_args()
+
+# Setup data and weights file path
+data_folder, weights_folder, logger_folder = get_folder_path(__model__, args.dataset + args.dataset_name)
+
+# Setup device
+if not torch.cuda.is_available() or args.device == 'cpu':
+    device = 'cpu'
+else:
+    device = 'cuda:{}'.format(args.gpu_idx)
+
+# Setup args
+dataset_args = {
+    'root': data_folder, 'dataset': args.dataset, 'name': args.dataset_name,
+    'num_core': args.num_core, 'step_length': args.step_length, 'train_ratio': args.train_ratio,
+    'debug': args.debug
+}
+model_args = {
+    'heads': args.heads, 'hidden_size': args.hidden_size, 'emb_dim': args.emb_dim,
+    'repr_dim': args.repr_dim
+}
+train_args = {
+    'debug': args.debug, 'runs': args.runs,
+    'model': __model__,
+    'opt': args.opt, 'loss': args.loss,
+    'epochs': args.epochs, 'batch_size': args.batch_size,
+    'weight_decay': args.weight_decay, 'lr': args.lr, 'device': device,
+    'weights_folder': weights_folder, 'logger_folder': logger_folder}
+print('dataset params: {}'.format(dataset_args))
+print('task params: {}'.format(model_args))
+print('train params: {}'.format(train_args))
+
+if __name__ == '__main__':
