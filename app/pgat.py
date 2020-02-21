6ea2073a6adc61a587c40808d0e5bece360199c6
Zhiwei Han <hanzw356255531@icloud.com>
2020-02-21 09:26:16 +0100

    sync

--- app/pgat.py
+++ app/pgat.py
@@ -1,31 +1,20 @@
 import torch
 import torch.nn.functional as F
 import torch
-from torch_geometric.nn import GATConv, PAConv
+from torch_geometric.nn import PAGATConv
 
 
 class PAGATNet(torch.nn.Module):
-    def __init__(self, num_nodes, emb_dim, repr_dim, heads=4, dropout=0.6):
+    def __init__(self, emb_dim, repr_dim, heads=4, dropout=0.6):
         super(PAGATNet, self).__init__()
         self.emb_dim = emb_dim
         self.repr_dim = repr_dim
 
-        self.node_emb = torch.nn.Embedding(num_nodes, emb_dim, max_norm=1, norm_type=2.0)
-
-        self.conv = PAConv(emb_dim, repr_dim, heads=heads, dropout=dropout)
+        self.conv = PAGATConv(emb_dim, repr_dim, heads=heads, dropout=dropout)
 
     def reset_parameters(self):
         self.conv.reset_parameters()
 
-    def forward_(self, x, path):
+    def forward(self, x, path):
         return self.conv(x, path)
-
-    def forward(self, path):
-        '''
-
-        :param edge_index: np.array, [2, N]
-        :param sec_order_edge_index: [3, M]
-        :return:
-        '''
-        return self.forward_(self.node_emb.weight, path)
 
--- app/pgat_recsys.py
+++ app/pgat_recsys.py
@@ -6,11 +6,13 @@ import torch
 import os.path as osp
 import operator
 
-from torch_geometric.utils import path
 
 from torch_geometric.datasets import MovieLens
 
-from .pgat import PAGATNet
+from pgat import PAGATNet
+from torch_geometric.datasets import MovieLens
+from torch_geometric import utils
+from utils import get_folder_path
 
 
 class PGATRecSys(object):
@@ -20,7 +22,7 @@ class PGATRecSys(object):
 
         self.data = MovieLens(**dataset_args).data.to(device_args['device'])
 
-        model_args['num_nodes'] = self.data.num_nodes[0]
+        self.node_emb = torch.nn.Embedding(self.data.num_nodes[0], model_args['emb_dim'], max_norm=1, norm_type=2.0)
         self.model = PAGATNet(**model_args).to(device_args['device'])
 
     def get_top_n_popular_items(self, n=10):
@@ -50,18 +52,18 @@ class PGATRecSys(object):
         self.base_iids = iids
         self.demographic_info = demographic_info
         # Build edges for new user
-        self.new_user_nid = self.model.node_emb.weight.shape[0]
-        new_user_gender_nid = self.data.gender2nid[0][demographic_info[0]]
-        new_user_occ_nid = self.data.occ2nid[0][demographic_info[1]]
+        self.new_user_nid = self.node_emb.weight.shape[0]
+        new_user_gender_nid = self.data.e2nid[0]['gender'][demographic_info[0]]
+        new_user_occ_nid = self.data.e2nid[0]['occ'][demographic_info[1]]
         row = [self.new_user_nid for i in range(len(iids) + 2)]
         col = iids + [new_user_gender_nid, new_user_occ_nid]
-        self.new_edge_index = torch.from_numpy(np.array([row + col, col + row])).long().to(self.train_args['device'])
+        self.new_edge_index = torch.from_numpy(np.array([row + col, col + row])).long().to(self.device_args['device'])
 
         # Build path begins and ends with
-        path_from_new_user_np = path.join(self.new_edge_index, self.data.edge_index)
-        path_to_new_user_np = path.join(self.data.edge_index, self.new_edge_index)
+        path_from_new_user_np = utils.path.join(self.new_edge_index, self.data.edge_index)
+        path_to_new_user_np = utils.path.join(self.data.edge_index, self.new_edge_index)
         new_path_np = np.concatenate([path_from_new_user_np, path_to_new_user_np], axis=1)
-        self.new_path = torch.from_numpy(new_path_np).to(self.train_args['device'])
+        self.new_path = torch.from_numpy(new_path_np).long().to(self.device_args['device'])
         # new_edge_index_df = pd.DataFrame({'head': new_edge_index_np[0, :], 'middle': new_edge_index_np[1, :]})
         # edge_index_np = self.data.edge_index.numpy()
         # edge_index_df = pd.DataFrame({'middle': edge_index_np[0, :], 'tail': edge_index_np[1, :]})
@@ -70,11 +72,9 @@ class PGATRecSys(object):
         # new_sec_order_edge = torch.from_numpy(new_sec_order_edge_np).to(self.train_args['device']).t()
 
         # Get new user embedding by applying message passing
-        path = torch.from_numpy(self.data.path[0]).to(self.train_args['device'])
-        self.node_emb = self.model.forward_(self.model.node_emb.weight, path)
-        self.new_user_emb = torch.nn.Embedding(1, self.model.node_emb.weight.shape[1]).weight
-        node_emb = torch.cat((self.model.node_emb.weight, self.new_user_emb), dim=0)
-        self.new_user_emb, self.att_factor = self.model.forward_(node_emb, self.new_path)[-1, :]
+        self.new_user_emb = torch.nn.Embedding(1, self.node_emb.weight.shape[1], max_norm=1, norm_type=2.0).weight
+        new_node_emb = torch.cat((self.node_emb.weight, self.new_user_emb), dim=0)
+        self.new_user_emb, self.att_factor = self.model.forward(new_node_emb, self.new_path)[-1, :]
         print('user building done...')
 
     def get_recommendations(self, seen_iids):
@@ -83,7 +83,7 @@ class PGATRecSys(object):
         rec_iids = [iid for iid in iids if iid not in seen_iids]
         rec_iids = np.random.choice(rec_iids, 20)
         rec_nids = [self.data.e2nid[0]['iid'][iid] for iid in rec_iids]
-        rec_item_emb = self.node_emb[rec_nids]
+        rec_item_emb = self.node_emb.weight[rec_nids]
         est_feedback = torch.sum(self.new_user_emb * rec_item_emb, dim=1).reshape(-1).cpu().detach().numpy()
         rec_iid_idx = np.argsort(est_feedback)[:self.num_recs]
         rec_iids = rec_iids[rec_iid_idx]
@@ -109,3 +109,61 @@ class PGATRecSys(object):
         path = max(self.att_factor_dict.iteritems(), key=operator.itemgetter(1))[0]
         path_np = path.cpu().to_numpy()
         return str(path_np)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    default_poster_src = 'https://www.nehemiahmfg.com/wp-content/themes/dante/images/default-thumb.png'
+
+    ########################## Define arguments ##########################
+    # Dataset params
+    parser.add_argument("--dataset", type=str, default='movielens', help="")
+    parser.add_argument("--dataset_name", type=str, default='1m', help="")
+    parser.add_argument("--num_core", type=int, default=10, help="")
+    parser.add_argument("--step_length", type=int, default=2, help="")
+    parser.add_argument("--train_ratio", type=float, default=False, help="")
+    parser.add_argument("--debug", default=0.01, help="")
+
+    # Model params
+    parser.add_argument("--heads", type=int, default=4, help="")
+    parser.add_argument("--dropout", type=float, default=0.6, help="")
+    parser.add_argument("--emb_dim", type=int, default=64, help="")
+    parser.add_argument("--repr_dim", type=int, default=16, help="")
+
+    # Device params
+    parser.add_argument("--device", type=str, default='cpu', help="")
+    parser.add_argument("--gpu_idx", type=str, default='0', help="")
+
+    args = parser.parse_args()
+
+    # save id selected by users
+    iid_list = []
+    iid_list2 = []
+
+    ########################## Define arguments ##########################
+    data_folder, weights_folder, logger_folder = get_folder_path(args.dataset + args.dataset_name)
+
+    ########################## Setup Device ##########################
+    if not torch.cuda.is_available() or args.device == 'cpu':
+        device = 'cpu'
+    else:
+        device = 'cuda:{}'.format(args.gpu_idx)
+
+    ########################## Define parameters ##########################
+    dataset_args = {
+        'root': data_folder, 'dataset': args.dataset, 'name': args.dataset_name,
+        'num_core': args.num_core, 'step_length': args.step_length, 'train_ratio': args.train_ratio,
+        'debug': args.debug
+    }
+    model_args = {
+        'heads': args.heads, 'emb_dim': args.emb_dim,
+        'repr_dim': args.repr_dim, 'dropout': args.dropout
+    }
+    device_args = {'debug': args.debug, 'device': device, 'gpu_idx': args.gpu_idx}
+    print('dataset params: {}'.format(dataset_args))
+    print('task params: {}'.format(model_args))
+    print('device_args params: {}'.format(device_args))
+
+    recsys = PGATRecSys(num_recs=10, dataset_args=dataset_args, model_args=model_args, device_args=device_args)
+    recsys.build_user(list(range(10)), ('M', '1'))
