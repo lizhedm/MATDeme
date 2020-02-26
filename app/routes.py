84ebfc70d2fdb2f3c8342f36bf30c90063943e4e
lizhedm <lizhedm@icloud.com>
2020-02-26 02:59:45 +0100

    update question data save

--- app/routes.py
+++ app/routes.py
@@ -184,6 +184,35 @@ def save_question_result1_tosqlite(user_id,question_result1_list):
     connection.close()
     return 1
 
+@app.template_global()
+def save_question_result2_tosqlite(user_id,question_result2_list):
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "MATDemo.db")
+    connection = sqlite3.connect(db_path)
+
+    cursor = connection.cursor()
+    print("Opened database successfully")
+
+    cursor.execute('create table if not exists QUESTION_RESULT2 (user_id,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10)')
+    params = (user_id,
+              question_result2_list[0],
+              question_result2_list[1],
+              question_result2_list[2],
+              question_result2_list[3],
+              question_result2_list[4],
+              question_result2_list[5],
+              question_result2_list[6],
+              question_result2_list[7],
+              question_result2_list[8],
+              question_result2_list[9])
+
+    cursor.execute("INSERT INTO QUESTION_RESULT2 VALUES (?,?,?,?,?,?,?,?,?,?,?)", params)
+    connection.commit()
+    print("Records created successfully")
+
+    connection.close()
+    return 1
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -369,6 +398,15 @@ def score_movie_transfer3():
 @app.route('/user_feedback')
 def user_feedback():
     return render_template('user_feedback.html',title = 'Film Recommendation')
+
+@app.route('/question_result_transfer2',methods=['GET','POST'])
+def question_result_transfer2():
+    if request.method == 'POST':
+        user_id = request.values['user_id']
+        question_result_list = request.values['question_result_list']
+        if question_result_list != '':
+            save_question_result2_tosqlite(user_id,question_result_list)
+            return 'success'
 
 
 
